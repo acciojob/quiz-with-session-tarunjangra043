68f62220a0f966eeca21b8e827accd23f88d63e1
Tarun Jangra <111531292+tarunjangra043@users.noreply.github.com>
2024-09-02 22:16:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-//your JS code here.
-
-// Do not change code below this line
-// This code will just display the questions to the screen
 const questions = [
   {
     question: "What is the capital of France?",
@@ -73,5 +69,5 @@ document.getElementById('submit').addEventListener('click', function() {
     }
   }
   document.getElementById('score').innerText = 'Your score is ' + score + ' out of ' + questions.length + '.';
-  localStorage.setItem('score', score);
+  localStorage.setItem('score', score);
 });
